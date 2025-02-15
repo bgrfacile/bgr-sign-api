1d38c334d40268d8d5abaf06a96e01ddcd515f1f
benaja-bendo <benaja.bendo02@gmail.com>
2025-02-15 14:16:33 +0100

    init commit

---
+++ src/main/java/com/bgrfacile/bgrsignapi/BgrSignApiApplication.java
@@ -0,0 +1,13 @@
+package com.bgrfacile.bgrsignapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgrSignApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgrSignApiApplication.class, args);
+    }
+
+}
