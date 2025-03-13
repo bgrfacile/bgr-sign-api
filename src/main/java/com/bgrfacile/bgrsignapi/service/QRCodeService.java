9cb7482a3c55ef6bbd154c2c209e861739cb9053
benaja-bendo <benaja.bendo02@gmail.com>
2025-03-13 23:55:29 +0100

    ajout: ajout du QRCodeService qui permet de générer un qrcode

---
+++ src/main/java/com/bgrfacile/bgrsignapi/service/QRCodeService.java
@@ -0,0 +1,32 @@
+package com.bgrfacile.bgrsignapi.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class QRCodeService {
+
+    public String generateQRCodeBase64(String attendanceUrl, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.MARGIN, 0); // Supprime le padding (marge)
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(attendanceUrl, BarcodeFormat.QR_CODE, width, height, hints);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        return Base64.getEncoder().encodeToString(pngOutputStream.toByteArray());
+    }
+}
