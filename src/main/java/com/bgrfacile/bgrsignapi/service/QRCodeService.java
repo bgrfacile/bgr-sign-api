fd663b41124de4c43eb6c177b47e4c2f33bb8955
benaja-bendo <benaja.bendo02@gmail.com>
2025-03-14 00:07:55 +0100

    maj: maj de generateQRCodeBase64 pour avoir un 0 de margin

--- src/main/java/com/bgrfacile/bgrsignapi/service/QRCodeService.java
+++ src/main/java/com/bgrfacile/bgrsignapi/service/QRCodeService.java
@@ -3,11 +3,14 @@ package com.bgrfacile.bgrsignapi.service;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Service;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
@@ -19,13 +22,19 @@ public class QRCodeService {
 
     public String generateQRCodeBase64(String attendanceUrl, int width, int height) throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
         Map<EncodeHintType, Object> hints = new HashMap<>();
-        hints.put(EncodeHintType.MARGIN, 0); // Supprime le padding (marge)
+        hints.put(EncodeHintType.MARGIN, 0);  // Supprime les marges
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);  // Correction d'erreur élevée
 
         BitMatrix bitMatrix = qrCodeWriter.encode(attendanceUrl, BarcodeFormat.QR_CODE, width, height, hints);
 
+        // Configurer pour avoir un QR code sans bordure blanche
+        MatrixToImageConfig config = new MatrixToImageConfig(MatrixToImageConfig.BLACK, MatrixToImageConfig.WHITE);
+        BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix, config);
+
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
-        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        javax.imageio.ImageIO.write(qrImage, "PNG", pngOutputStream);
 
         return Base64.getEncoder().encodeToString(pngOutputStream.toByteArray());
     }
